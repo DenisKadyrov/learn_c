acd73054b738b127290e27574790a4a034c2c877
Denis <kripmrx@gmail.com>
2024-11-29 00:51:17 +0100

    change qsort for work with array of char poingers

--- qsort.c
+++ qsort.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 
 
-void qsort(int[], int, int);
 
-int main(void)
-{
-    // use example
-    int i;
-    int arr[] = {1, 5, 35, 6, 8, 32, 5, 57, 76, 3};
-    qsort(arr, 0, 10);
-    for (i = 0; i < 10; i++)
-        printf("%d\n", arr[i]);
-}
-
-/* function for sort int array of integer */
-void qsort(int v[], int left, int right)
+/* function for sort array of pointers to char */
+void qsort(char *v[], int left, int right)
 {
     int i, last;
-    void swap(int *, int *);
+    void swap(char *v[], int i, int j);
+    int strcmp(char *s, char *t);
     
     if (left >= right)
         return;
 
-    swap(&v[left], &v[(left + right)/2]);
+    swap(v, left, (left + right) / 2);
     last = left;
     for (i = left + 1; i <= right; i++)
-        if (v[i] < v[left])
-            swap(&v[++last], &v[i]);
-    swap(&v[last], &v[left]);
+        if (strcmp(v[i], v[left]) < 0)
+            swap(v, ++last, i);
+    swap(v, last, left);
     qsort(v, left, last-1);
     qsort(v, last+1, right);
 }
 
 /* function for swap 2 element of array */
-void swap(int *px, int *py)
+void swap(char *v[], int i, int j)
 {
-    int temp;
-    temp = *px;
-    *px = *py;
-    *py = temp;
+    char *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+int strcmp(char *s, char *t)
+{
+    for (; *s == *t; t++, s++)
+        if (*s = '\0')
+            return 0;
+    return *s - *t;
 }
