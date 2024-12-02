44db5e8771ddd1e53632a3619a88cf70c377344c
Denis <kripmrx@gmail.com>
2024-12-02 22:44:42 +0100

    update strindex func for work with command line parameters

--- strindex.c
+++ strindex.c
@@ -6,20 +6,20 @@
    }
 */
 #include <stdio.h>
+#include <string.h>
 
 #define MAXLINE 1000
 
 int getlin(char line[], int max); 
 int strindex(char source[], char searchfor[]);
 
-char pattern[] = "ould";
 
-int main()
+int main(int argc, char *argv[])
 {
    char line[MAXLINE];
    int found = 0;
    while (getlin(line, MAXLINE) > 0)
-       if (strindex(line, pattern) >= 0) {
+       if (strstr(line, argv[1]) > 0) {
            printf("%s", line);
            found++;
        }
@@ -38,14 +38,3 @@ int getlin(char s[], int lim)
     return i;
 }
 
-int strindex (char s[], char t[])
-{
-    int i, j, k;
-    for (i = 0; s[i] != '\0'; i++) {
-        for (j = i, k = 0; t[k] != '\0' && t[k] == s[j]; k ++, j ++)
-            ;
-        if (k > 0 && t[k] == '\0')
-            return i;
-    }
-    return -1;
-}
