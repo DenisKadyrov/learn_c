660d15aee4a003b1d01619263c449d80b8ca14ca
Denis <kripmrx@gmail.com>
2024-12-05 16:27:01 +0100

    uses pointers to structure

--- count_keywords.c
+++ count_keywords.c
@@ -44,43 +44,43 @@ struct key {
     "unsigned", 0,
 };
 int getword(char *, int);
-int binsearch(char *, struct key *, int);
+struct key *binsearch(char *, struct key *, int);
 
 
 int main()
 {
-    int n;
+    struct key *p;
     char word[MAXWORD];
 
     while (getword(word, MAXWORD) != EOF)
         if (isalpha(word[0]))
-            if ((n = binsearch(word, keytab, NKEY)) >= 0)
-                keytab[n].count++;
+            if ((p = binsearch(word, keytab, NKEY)) != NULL)
+                p->count++;
 
-    for (n = 0; n < NKEY; n++)
-        if (keytab[n].count > 0)
-            printf("%4d %s\n", keytab[n].count, keytab[n].word);
+    for (p = keytab; p < keytab + NKEY; p++)
+        if (p->count > 0)
+            printf("%4d %s\n", p->count, p->word);
     return 0;
 }
 
 /* binary search function */
-int binsearch(char *word, struct key tab[], int n)
+struct key *binsearch(char *word, struct key tab[], int n)
 {
     int cond;
-    int low, mid, high;
+    struct key *low = &tab[0];
+    struct key *high = &tab[n];
+    struct key *mid;
 
-    low = 0;
-    high = n - 1; 
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if ((cond = strcmp(word, tab[mid].word)) < 0)
-            high = mid - 1;
+    while (low < high) {
+        mid = low + (high - low) / 2;
+        if ((cond = strcmp(word, mid->word)) < 0)
+            high = mid;
         else if (cond > 0)
             low = mid + 1;
         else 
             return mid;
     }
-    return -1;
+    return NULL;
 }
 
 /* function for get a word */
